d95e06fca7555c8516469435dc42234af2d61108
Alvee3120 <fazlahkarim2002@gmail.com>
2025-08-08 22:13:59 +0600

    Updated something

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from "./components/Header/Header"
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className=' text-4xl bg-amber-300'> Hello</h1>
+   
+    </>
+  )
+}
+
+export default App
--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -16,6 +16,8 @@ function App() {
   const [icon, seticon] = useState('')
   const [main, setMain] = useState('')
   const [humidity, setHumidity] = useState('')
+   const [cod, setCod] = useState('')
+  //  const [found ,setfound] = useState(false) 
 
 
   const handleSubmit = (e) => {
@@ -27,7 +29,9 @@ function App() {
         return response.json();
       }).then((data) => {
 
-
+       
+        console.log(data);
+        // setCod(data.cod)
         console.log(data.name);
         setTemp(Math.round(data.main.temp - 273.15))
         setFeelslike(Math.round(data.main.feels_like - 273.15))
@@ -58,7 +62,7 @@ function App() {
         <div className="absolute inset-0 bg-black opacity-60 z-0 "></div>
 
         {/* Main Card */}
-        <div className=" md:w-[50vw] w-[80vw]  rounded-2xl h-[80vh] shadow-2xl bg-[#382866] relative z-10">
+        <div className=" md:w-[60vw] w-[80vw]  rounded-2xl h-[80vh] shadow-2xl bg-[#382866] relative z-10">
 
           <div className="  text-4xl font-bold mx-auto mt-4 w-fit text-white">
             <h2>Weather App</h2>
@@ -78,25 +82,26 @@ function App() {
           </div>
 
           {/* result section */}
-
-          {showResult && (<div id='result' className='mt-6'>
-<h2 className='mt-3 text-center text-3xl font-normal text-white'>{displaycity}, {country}</h2>
+          
+         
+            {showResult && (<div id='result' className='mt-6'>
+            <h2 className='mt-3 text-center text-3xl font-normal text-white'>{displaycity}, {country}</h2>
             <div className='flex flex-wrap items-center justify-center text-center'>
               <img src={`https://openweathermap.org/img/wn/${icon}.png`} alt="icon" className='w-20' />
               <h2 className=' text-center text-6xl font-bold text-white'>{temp}°C</h2>
             </div>
 
             <h2 className='mt-3 text-center text-3xl font-normal text-white'>{main}</h2>
-            
 
 
-            <div id="subresult" className=" flex gap-16 justify-between  m-12 text-white mt-6">
-              <div id="item1" className="flex flex-col gap-2">
+
+            <div id="subresult" className="flex flex-col md:flex-row  gap-16 justify-between m-12 text-white mt-6">
+              <div id="item1" className="flex flex-col gap-2 md:text-2xl text-[20px]">
                 <p>Feels Like: {feelslike}°C</p>
                 <p>Minimum Temperature: {mintemp}°C</p>
                 <p>Maximum Temperature: {maxtemp}°C</p>
               </div>
-              <div id="item2" className="flex flex-col gap-2">
+              <div id="item2" className="flex flex-col gap-2 md:text-2xl text-[20px]">
                 <p>Pressure: {pressure} hPa</p>
                 <p>Wind Speed: {windspeed} m/s</p>
                 <p>Humidity: {humidity}%</p>
@@ -104,7 +109,11 @@ function App() {
             </div>
 
 
+
           </div>)}
+         
+
+          
         </div>
       </div >
 
